887d4e22bdd390c5c622f50b236c02e35bf7cc5b
Эдуард Шамсиев <s-6001@yandex.ru>
2022-01-18 14:47:34 +0500

    Добавьте файлы проекта.

---
+++ Wpf_task_6/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+
+namespace Wpf_task_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        class WeatherControl
+        {
+            public static readonly DependencyProperty TemperatureProperty =
+                DependencyProperty.Register(
+                    nameof(Temperature),
+                    typeof(string),
+                    typeof(MainWindow),
+                    new PropertyMetadata(""));
+            private int temperature;
+            private string windDirection;
+            private int windSpeed;
+            private int rainfall;
+            public int Temperature
+            {
+                get
+                {
+                    return this.temperature;
+                }
+                set
+                {
+                    if (value < -50)
+                        temperature = -50;
+                    else if (value > 50)
+                        temperature = 50;
+                    else
+                        this.temperature = value;
+                }
+            }
+            public string WindDirection { get; set; }
+            public int WindSpeed { get; set; }
+            private enum Rainfall : int { солнечно, облачно, дождь, снег }
+            public WeatherControl(int temperature, string windDirection, int windSpeed, int rainfall)
+            {
+                this.temperature = temperature;
+                this.windDirection = windDirection;
+                this.windSpeed = windSpeed;
+                this.rainfall = rainfall;
+            }
+            public string Print()
+            {
+                return string.Format("Температура - {0}, направление ветра - (1), скорость ветра - {2}, {3}", temperature, windDirection, windSpeed, rainfall);
+            }
+        }
+    }
+}
